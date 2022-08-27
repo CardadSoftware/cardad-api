753aadf69ceb76a375f966d0577be11efb211cc4
Anthony Stoute <crackpotmgee@gmail.com>
2022-08-27 18:03:19 +0000

    working db connection

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,22 @@
-const { VehicleModel } = require('../../cardad-db/cardadSchema');
-const mongoose = require('mongoose');
+const { VehicleModel, db } = require('../../cardad-db/cardadSchema');
 
 //Set up default mongoose connection
-const mongoDB = 'mongodb://127.0.0.1/cardad';
+const mongoDB = 'mongodb://127.0.0.1:27017';
 
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, user: "crackpotmgee", pass: "rP&rs9g89qn7"});
+db.connect(mongoDB, { dbName:"cardad", useNewUrlParser: true, useUnifiedTopology: true, user: "cardadAPI", pass: "rP&7ZxRz63uEsPe1cq426R9"},(err) => {if(err){console.log("Enable to connect to DB: " + err.message + " stack: " + err.stack);} else{console.log("Connected to DB");}});
 
 var express = require('express');
 var router = express.Router();
 
-router.post('/vehicles', function (req, res, next) {
+router.post('/vehicles/getVehicle', function (req, res, next) {
   var body = req.body;
-  var result = VehicleModel.findOne({name: body.name});
-  res.json(result);
+  VehicleModel.find({name: new RegExp(body.name,'i')}).exec().then(results => res.json(results));
+  
+});
+
+router.post('/vehicles/addVehicle', function (req, res, next) {
+  VehicleModel.create(req.body).then(saved => res.json(saved));
+  
 });
 
 module.exports = router;
