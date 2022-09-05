deeb870f6de8d2a38ff56ebb16cc9b68b2aa0ca0
Anthony Stoute <crackpotmgee@gmail.com>
2022-09-05 03:16:57 +0000

    added create charge action

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-const { VehicleModel} = require('../../cardad-db/cardadSchema');
+const { VehicleModel, ChargeModel} = require('../../cardad-db/cardadSchema');
 
 var express = require('express');
 var router = express.Router();
@@ -12,6 +12,23 @@ router.post('/vehicles/getVehicle', function (req, res, next) {
 router.post('/vehicles/addVehicle', function (req, res, next) {
   VehicleModel.create(req.body).then(saved => res.json(saved));
   
+});
+
+router.post('/charge/createCharge', function (req, res, next)
+{
+  var newCharge = new ChargeModel(req.body);
+  newCharge.validate().then((err) => 
+  {
+    if(err){
+      res.statusCode = 400;
+      res.json(err.message);
+    }
+    newCharge.save().then((saved) => res.json(saved)).catch((err) => 
+    {
+    res.statusCode = 400;
+    res.json(err)
+    });
+  });
 });
 
 module.exports = router;
